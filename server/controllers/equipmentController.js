1d4e713b6e4b269a671798215861a7d6a9ca34d6
Missiadar <danila-kholkin@mail.ru>
2024-05-18 23:50:06 +0300

    add routing

---
+++ server/controllers/equipmentController.js
@@ -0,0 +1,15 @@
+class EquipmentController {
+    async create(req, res){
+
+    }
+
+    async getAll(req, res){
+
+    }
+
+    async getOne(req, res){
+
+    }
+}
+
+module.exports = new EquipmentController()

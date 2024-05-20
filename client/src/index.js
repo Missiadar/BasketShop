dd3e43fa338c07336815a2a556d8ee2e8744e130
Missiadar <danila-kholkin@mail.ru>
2024-05-20 21:49:52 +0300

    added userstore and fixed warnings

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import UserStore from './store/UserStore';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+export const Context = createContext(null)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Context.Provider value={{
+    user: new UserStore()
+  }}>
+    <App />
+  </Context.Provider>
 );
